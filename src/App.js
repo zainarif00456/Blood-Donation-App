7576dec8ef44a251b0cab9db8cd5c88ca722f43b
Zain Ul Abdeen <66191019+zainarif00456@users.noreply.github.com>
2022-11-15 16:46:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,23 +25,6 @@ function App() {
   
   return (
     <>
-    {/* <div className="container">
-  <div className="mb-3">
-    <label htmlFor="InputEmail1" className="form-label">Email address</label>
-    <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} 
-    className="form-control" id="InputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="InputPassword1" className="form-label">Password</label>
-    <input type="password" value={passwd} className="form-control" id="InputPassword1" onChange={(e)=>{setpasswd(e.target.value)}} />
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="Check1"/>
-    <label className="form-check-label" htmlFor="Check1">Check me out</label>
-  </div>
-  <button type="button" onClick={register} className="btn btn-primary">Submit</button>
-  </div> */}
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-2"></div>
